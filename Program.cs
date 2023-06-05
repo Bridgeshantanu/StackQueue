5aa58b70f78641794187191ea17de448384e56c9
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-05 13:57:02 +0530

    Add: push element code is added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructure1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            
+        }
+
+    }
+}
